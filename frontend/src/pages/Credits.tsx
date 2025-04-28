aea69f20d6fe89d4045d7e29aa96cc72e00d66dc
TomDubois <tom.dubois12@gmail.com>
2025-04-28 11:22:38 +0200

    Ajout d'un bouton retour + resutructation et styles de la page des crédits

--- frontend/src/pages/Credits.tsx
+++ frontend/src/pages/Credits.tsx
@@ -4,42 +4,56 @@ import styles from "../styles/pages/Credits.module.scss"
 
 import Header from "../components/Header";
 import Title from "../components/Title";
+import Button from "../components/Button";
 
 export default function Credits() {
 
     const { t } = useTranslation();
     
+    function goBack() {
+        window.history.back();
+    }
+    
     return (
         <>
             <Header text={t("Parametre.Credits")} />
-            <div className={styles.title}>
-                <Title text={t("Credits.Title.Credit")} />
-            </div>
-            <div className={styles.txt}>
-                <p>{t("Credits.CreditTxt")}</p>
-            </div>
-            <div className={styles.title}>
-                <Title text={t("Credits.Title.Objectif")} />
-            </div>
-            <div className={styles.txt}>
-                <p>{t("Credits.ObjectifTxt")}</p>
-            </div>
-            <div className={styles.title}>
-                <Title text={t("Credits.Title.TechUse")} />
-            </div>
-            <div className={styles.txt}>
-                <li>{t("Credits.TechUse1")}</li>
-                <li>{t("Credits.TechUse2")}</li>
-            </div>
-            <div className={styles.title}>
-                <Title text={t("Credits.Title.Contact")} />
-            </div>
-            <div className={styles.txt}>
-                <p>{t("Credits.ContactTxt")}</p>
-                <li>{t("Credits.Contact1")}</li>
-            </div>
-            <div className={styles.thx}>
-                <p>{t("Credits.FinalTxt")}</p>
+            <div className={styles.containerCredit}>
+                <div className={styles.title}>
+                    <Title text={t("Credits.Title.Credit")} />
+                </div>
+                <div className={styles.txt}>
+                    <p>{t("Credits.CreditTxt")}</p>
+                </div>
+                <div className={styles.title}>
+                    <Title text={t("Credits.Title.Objectif")} />
+                </div>
+                <div className={styles.txt}>
+                    <p>{t("Credits.ObjectifTxt")}</p>
+                </div>
+                <div className={styles.title}>
+                    <Title text={t("Credits.Title.TechUse")} />
+                </div>
+                <div className={styles.txt}>
+                    <li>{t("Credits.TechUse1")}</li>
+                    <li>{t("Credits.TechUse2")}</li>
+                </div>
+                <div className={styles.title}>
+                    <Title text={t("Credits.Title.Contact")} />
+                </div>
+                <div className={styles.txt}>
+                    <p>{t("Credits.ContactTxt")}</p>
+                    <li>{t("Credits.Contact1")}</li>
+                </div>
+                <div className={styles.thx}>
+                    <p>{t("Credits.FinalTxt")}</p>
+                </div>
+                <Button 
+                    className={styles.buttonCredit}
+                    backgroundColor={"#2B3244"}
+                    text={t("Rapport.GoBack")} 
+                    textColor={"white"} 
+                    onClick={goBack}
+                />
             </div>
         </>
     );
