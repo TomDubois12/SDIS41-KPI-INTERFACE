bab494d23446bbb06442fee3ad3f6d8e879d962d
TomDubois <tom.dubois12@gmail.com>
2025-04-29 10:46:37 +0200

    Correction des images flous + ajout des images manquantes

--- frontend/src/pages/Alertes.tsx
+++ frontend/src/pages/Alertes.tsx
@@ -2,7 +2,8 @@ import { useTranslation } from "../hooks/useTranslation";
 
 import Header from "../components/Header";
 import Card from "../components/Card";
-import Feu from '../assets/images/Feu.png';
+import Pompier1 from '../assets/images/Pompier1.png';
+import Pompier2 from '../assets/images/Pompier2.png';
 
 import styles from "../styles/pages/Alertes.module.scss"
 
@@ -15,13 +16,13 @@ export default function Alertes() {
             <Header text={t("Titles.Alertes")} />  
             <div className={styles.divCard}>    
                 <Card 
-                    img={Feu} //changer les images
+                    img={Pompier1} //changer les images
                     text={t("EmailOnduleur.AlerteTitle")} 
                     buttonText={t("Global.Entrer")}
                     path="/alertes_onduleurs"
                 />
                 <Card 
-                    img={Feu} //changer les images
+                    img={Pompier2} //changer les images
                     text={t("EmailINPT.AlerteTitle")}
                     buttonText={t("Global.Entrer")}
                     path="/alertes_inpt"
